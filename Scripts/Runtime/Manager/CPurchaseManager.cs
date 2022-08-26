66fe01b3943ac197115f69c874b3e5a7722d5163
sd.lee <sd.lee@ninetap.com>
2022-08-27 00:43:09 +0900

    [이상동] 테이블 관련 구문 수정

--- Scripts/Runtime/Manager/CPurchaseManager.cs
+++ Scripts/Runtime/Manager/CPurchaseManager.cs
@@ -348,16 +348,12 @@ public partial class CPurchaseManager : CSingleton<CPurchaseManager>, IStoreList
 
 	/** 결제 상품 식별자를 로드한다 */
 	private List<string> LoadPurchaseProductIDs() {
-#if MSG_PACK_ENABLE
 		return CFunc.ReadMsgPackObj<List<string>>(KCDefine.U_DATA_P_PURCHASE_PRODUCT_IDS, true);
-#endif			// #if MSG_PACK_ENABLE
 	}
 
 	/** 결제 상품 식별자를 저장한다 */
 	private void SavePurchaseProductIDs(List<string> a_oPurchaseProductIDList) {
-#if MSG_PACK_ENABLE
 		CFunc.WriteMsgPackObj<List<string>>(KCDefine.U_DATA_P_PURCHASE_PRODUCT_IDS, a_oPurchaseProductIDList, true);
-#endif			// #if MSG_PACK_ENABLE
 	}
 
 	/** 결제 결과를 처리한다 */
